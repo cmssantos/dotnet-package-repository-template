07c36eedf842756f5050af356086cf26c232aef4
Cleudice Santos <cleudice.ms@gmail.com>
2025-02-05 23:08:29 -0300

    Add tests

---
+++ tests/Dotnet.Package.Repository.Template.Tests/MyClassUnitTest.cs
@@ -0,0 +1,17 @@
+﻿namespace Dotnet.Package.Repository.Template.Tests;
+
+public class MyClassUnitTest
+{
+    [Fact]
+    public void GetName_ReturnsClassName()
+    {
+        // Arrange
+        var expected = "MyClass";
+
+        // Act
+        var actual = MyClass.GetName();
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+}
