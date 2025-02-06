ed6875cadd315d3ce4b4c505cbde95e127e444da
Cleudice Santos <cleudice.ms@gmail.com>
2025-02-05 22:57:04 -0300

    Add package example

---
+++ src/Dotnet.Package.Repository.Template/MyClass.cs
@@ -0,0 +1,11 @@
+﻿namespace Dotnet.Package.Repository.Template;
+
+public static class MyClass
+{
+    private const string CLASS_NAME = "MyClass";
+
+    public static string GetName()
+    {
+        return CLASS_NAME;
+    }
+}
